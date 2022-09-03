d35abbe4d3c022d4100e788bdd6a2f1c0c4fa04b
ismail-safha <ismailsafha@gmail.com>
2022-09-03 01:58:00 +0100

    project

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let panels = document.querySelectorAll(".panel");
+panels.forEach((e) => {
+  e.addEventListener("click", () => {
+    removeActiveClasses();
+    e.classList.add("active");
+  });
+});
+function removeActiveClasses() {
+  panels.forEach((e) => {
+    e.classList.remove("active");
+  });
+}
